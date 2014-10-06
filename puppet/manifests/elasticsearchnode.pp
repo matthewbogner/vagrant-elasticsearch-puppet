b34a2f6009f44f04d43c6b0736218fafe4cf354a
Keerthi Iyengar <keerthi.r.iyengar@gmail.com>
2014-10-06 17:55:40 +1100

    Added todo for full ELK stack. Fixed issues with seeds

--- puppet/manifests/elasticsearchnode.pp
+++ puppet/manifests/elasticsearchnode.pp
@@ -1,4 +1,6 @@
-
+# TODO: Classifying the nodes allows future provision for the full ELK stack to be provisioned. This should at some point be part of the Vagrant setup
 node 'default' {
-	class {'elasticsearch': }
+	class {'elasticsearch': 
+		seeds => "$seeds",
+	}
 }
