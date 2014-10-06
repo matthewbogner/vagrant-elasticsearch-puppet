794f1f4058f0defa7d505040ff1e7197a5439e52
Keerthi Iyengar <keerthi.r.iyengar@gmail.com>
2014-10-06 17:46:34 +1100

    Updated for support for Elasticsearch 1.3  - Updated Readme  - Added port forwarding for UI for each host  - Improved Puppet manifests to be declarative by removing "Exec" commands  - Removed need for any bash bootstrap scripts for repo setup  - Added version flag in puppet code  - Fixed bug that the service was not starting correctly

--- puppet/manifests/elasticsearchnode.pp
+++ puppet/manifests/elasticsearchnode.pp
@@ -1,11 +1,4 @@
-node 'esnode1' {
-	class {'elasticsearch': }
-}
-
-node 'esnode2' {
-        class {'elasticsearch': }
-}
 
 node 'default' {
-	notice "Host not found"
+	class {'elasticsearch': }
 }
--- puppet/modules/elasticsearch/manifests/init.pp
+++ puppet/modules/elasticsearch/manifests/init.pp
@@ -1,100 +1,46 @@
-class elasticsearch($version = "0.19.10", $seeds = "") {
-
+class elasticsearch($seeds = "") {
+	$es_version = "1.3"
+	# As good practice just incase there are any exec statements or cache directories required
 	Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
-
 	$tmpDir = "/tmp/elasticsearch"
-
-	package { 'java-1.6.0-openjdk': 
-	  ensure => present,
-	}
-	->
-	package { 'unzip':
-	  ensure => present,
-	}
-	->
+	
 	file { "$tmpDir":
 	  ensure => directory,
 	}
-	->
-	user { 'elasticsearch':
-      ensure     => present,
-      uid        => '507',
-      home       => "/usr/local/share/elasticsearch-$version",
-    }
-	->
-	exec { 'es-download-tarball':
-      command => "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$version.tar.gz -O elasticsearch.tar.gz",
-      cwd => "$tmpDir",
-      creates => "$tmpDir/elasticsearch.tar.gz"
-    }
-    ->
-    exec { 'es-untar':
-      command => 'tar -xf elasticsearch.tar.gz',
-      cwd => "$tmpDir",
-      creates => "$tmpDir/elasticsearch-$version"
-    }
-    ->
-    exec { 'es-move-binaries':
-      command => "cp -rp elasticsearch-$version /usr/local/share",
-      cwd => "$tmpDir",
-      creates => "/usr/local/share/elasticsearch-$version",
-    }
-    ->
-    exec { 'es-download-servicewrapper': 
-      command => 'curl -L http://github.com/elasticsearch/elasticsearch-servicewrapper/tarball/master | tar -xz',
-      cwd => "$tmpDir",
-      unless => "ls | grep servicewrapper",
-    }
-    ->
-	exec { 'es-move-servicewrapper':
-	  command => "cp -rp *servicewrapper*/service /usr/local/share/elasticsearch-$version/bin/",
-	  cwd => "$tmpDir",
-	  creates => "/usr/local/share/elasticsearch-$version/bin/service",
+
+	# Set up yum repo for elastic search as this is now supported
+	file { "/etc/yum.repos.d/elasticsearch.repo":
+	  content => template('elasticsearch/elasticsearch.repo.erb'),
 	}
-	->
-	exec { 'es-install-servicewrapper': 
-	  command => "/usr/local/share/elasticsearch-$version/bin/service/elasticsearch install && touch /usr/local/share/elasticsearch-$version/bin/service/service.installed",
-	  creates => "/usr/local/share/elasticsearch-$version/bin/service/service.installed",
+
+	# Note that elasticsearch 1.3 onwards only supports Java 7
+	# Verify that update-alternatives is used just in case there is a version mismatch
+	# Tested against a base box with no issues
+
+	package { ["java-1.7.0-openjdk","elasticsearch"]: 
+	  ensure => present,
+	  require => File["/etc/yum.repos.d/elasticsearch.repo"],
 	}
-	->
-	file { "/usr/local/share/elasticsearch-$version/config/elasticsearch.yml":
+	
+	file { "/etc/elasticsearch/elasticsearch.yml":
 	  content => template('elasticsearch/elasticsearch.yml.erb'),
+	  require => Package['elasticsearch'],
 	}
-	->
-	exec { 'es-servicewrapper-runasuser':
-	  command => "sed -i -e 's/#RUN_AS_USER=/RUN_AS_USER=elasticsearch/' /usr/local/share/elasticsearch-$version/bin/service/elasticsearch",
-	  onlyif => "cat /usr/local/share/elasticsearch-$version/bin/service/elasticsearch | grep '#RUN_AS_USER'",
-	}
-	->
-	exec { 'es-servicewrapper-ulimit':
-	  command => "sed -i -e 's/#ULIMIT_N=/ULIMIT_N=64000/' /usr/local/share/elasticsearch-$version/bin/service/elasticsearch",
-	  onlyif => "cat /usr/local/share/elasticsearch-$version/bin/service/elasticsearch | grep '#ULIMIT_N'",
-	}
-	->
+	
 	file { '/etc/security/limits.d/80-nofile.conf':
-	  content => template('elasticsearch/80-nofile.conf')
+	  content => template('elasticsearch/80-nofile.conf'),
+	  require => Package['elasticsearch'],
 	}
-	->
-	exec { 'es-install-head-plugin': 
-	  command => "/usr/local/share/elasticsearch-$version/bin/plugin -install mobz/elasticsearch-head",
-	  creates => "/usr/local/share/elasticsearch-$version/plugins/head",
-	}
-	->
-	exec { 'es-install-paramedic-plugin':
-	  command => "/usr/local/share/elasticsearch-$version/bin/plugin -install karmi/elasticsearch-paramedic",
-	  creates => "/usr/local/share/elasticsearch-$version/plugins/paramedic",
-	}
-	->
-	exec { 'es-chown-everything':
-        command => "chown -R elasticsearch:elasticsearch /usr/local/share/elasticsearch-$version/",
-    }
-	->
-	service { 'elasticsearch':
+	
+		service { 'elasticsearch':
 	  ensure => running,
+	  subscribe  => File['/etc/elasticsearch/elasticsearch.yml'],
+	  require => Package['elasticsearch'],
 	}
-	->
+
 	service { 'iptables': 
 	  ensure => stopped,      #HACK!  use the puppet firewall module to manage iptables 
+	  require => Service['elasticsearch'],
 	}
 
 }
